040235d29ef9607671e0076e6d9afb5115842a6b
Amanullah-Nirob <khannirub55@gmail.com>
2022-10-26 18:25:20 +0600

    backend url change

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: ['res.cloudinary.com','localhost'],
   },
   env: {
-    API_URL:'https://aman-chat-backend.vercel.app',
+    API_URL:'https://glacial-river-12489.herokuapp.com',
     DEFAULTImage:'https://res.cloudinary.com/abhi-sawant/image/upload/v1653670527/user_dqzjdz.png'
 },
 }
