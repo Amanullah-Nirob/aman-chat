04067f29c816407d4bd90e2ef60f41819e1b83ce
Amanullah-Nirob <khannirub55@gmail.com>
2022-12-22 13:30:51 +0600

    turn server configer

--- src/webRTC/webRTC.ts
+++ src/webRTC/webRTC.ts
@@ -56,7 +56,35 @@ export const newPeerConnection = (initiator: boolean) => {
     const peer = new Peer({
         initiator: initiator,
         trickle: false,
-        config: configuration,
+        config: {
+            iceServers: [
+              {
+                url: 'turn:numb.viagenie.ca',
+                credential: 'muazkh',
+                username: 'webrtc@live.com'
+              },
+              {
+                url: 'turn:192.158.29.39:3478?transport=udp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+              },
+              {
+                url: 'turn:192.158.29.39:3478?transport=tcp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+              },
+              {
+                url: 'turn:turn.bistri.com:80',
+                credential: 'homeo',
+                username: 'homeo'
+              },
+              {
+                url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+                credential: 'webrtc',
+                username: 'webrtc'
+              }
+            ]
+        },
         stream: stream,
     });
 
