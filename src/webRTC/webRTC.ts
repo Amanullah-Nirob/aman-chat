f8f4705b3c9db2fa451b099f5d2fdd330fb6c70e
Amanullah-Nirob <khannirub55@gmail.com>
2022-12-22 16:09:19 +0600

    try to fix

--- src/webRTC/webRTC.ts
+++ src/webRTC/webRTC.ts
@@ -27,13 +27,21 @@ const peerConfiguration = () => {
     const turnIceServers = null; 
 
     if (turnIceServers) {
-        // TODO use TURN server credentials
+        
     } else {
         console.warn("Using only STUN server");
         return {
             iceServers: [
+                // Test some STUN server
                 {
-                    urls: "stun:stun.l.google.com:19302",
+                    urls: 'stun:stun.l.google.com:19302',
+                },
+                // Test some TURN server
+                // https://www.metered.ca/tools/openrelay/
+                {
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
                 },
             ],
         };
@@ -56,36 +64,7 @@ export const newPeerConnection = (initiator: boolean) => {
     const peer = new Peer({
         initiator: initiator,
         trickle: false,
-        config: {
-            iceServers: [
-            { urls: "stun:numb.viagenie.ca:3478" },
-              {
-                url: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com'
-              },
-              {
-                url: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-              },
-              {
-                url: 'turn:192.158.29.39:3478?transport=tcp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-              },
-              {
-                url: 'turn:turn.bistri.com:80',
-                credential: 'homeo',
-                username: 'homeo'
-              },
-              {
-                url: 'turn:turn.anyfirewall.com:443?transport=tcp',
-                credential: 'webrtc',
-                username: 'webrtc'
-              }
-            ]
-        },
+        config: configuration,
         stream: stream,
     });
 
